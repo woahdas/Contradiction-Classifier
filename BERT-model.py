d7aa4f1f973b84292c2b7ed1adda635dff3caf4e
woahdas <78304648+woahdas@users.noreply.github.com>
2021-03-06 20:22:49 +0000

    Create BERT-model.py

---
+++ BERT-model.py
@@ -0,0 +1,98 @@
+#again, Kaggle wouldn't download anything from external sources, so I couldn't run this on Kaggle, but tried it on Pycharm with a different version and it worked.
+import numpy as np  # linear algebra
+import pandas as pd  # data processing, CSV file I/O (e.g. pd.read_csv)
+from transformers import BertTokenizer, TFBertModel
+import matplotlib.pyplot as plt
+import tensorflow as tf
+
+# TPU
+try:
+    tpu = tf.distribute.cluster_resolver.TPUClusterResolver()
+    tf.config.experimental_connect_to_cluster(tpu)
+    tf.tpu.experimental.initialize_tpu_system(tpu)
+    strategy = tf.distribute.experimental.TPUStrategy(tpu)
+except ValueError:
+    strategy = tf.distribute.get_strategy()  # for CPU and single GPU
+    print('No. of replicas:', strategy.num_replicas_in_sync)
+
+# getting the data
+# 0 is entailment, 1 is neutral, 2 is contradiction
+
+train = pd.read_csv("/kaggle/input/contradictory-my-dear-watson/train.csv")
+
+# getting the BERT model
+model_name = 'bert-base-multilingual-cased'
+tokenizer = BertTokenizer.from_pretrained(model_name)
+
+
+# tokenizing the data
+def encode_sentence(s):
+    tokens = list(tokenizer.tokenize(s))
+    tokens.append('[SEP]')
+    return tokenizer.convert_tokens_to_ids(tokens)
+
+
+def bert_encode(hypotheses, premises, tokenizer):
+    num_examples = len(hypotheses)
+
+    sentence1 = tf.ragged.constant([
+        encode_sentence(s)
+        for s in np.array(hypotheses)])
+    sentence2 = tf.ragged.constant([
+        encode_sentence(s)
+        for s in np.array(premises)])
+
+    cls = [tokenizer.convert_tokens_to_ids(['[CLS]'])] * sentence1.shape[0]
+    input_word_ids = tf.concat([cls, sentence1, sentence2], axis=-1)
+
+    input_mask = tf.ones_like(input_word_ids).to_tensor()
+
+    type_cls = tf.zeros_like(cls)
+    type_s1 = tf.zeros_like(sentence1)
+    type_s2 = tf.ones_like(sentence2)
+    input_type_ids = tf.concat(
+        [type_cls, type_s1, type_s2], axis=-1).to_tensor()
+
+    inputs = {
+        'input_word_ids': input_word_ids.to_tensor(),
+        'input_mask': input_mask,
+        'input_type_ids': input_type_ids}
+
+    return inputs
+
+
+train_input = bert_encode(train.premise.values, train.hypothesis.values, tokenizer)
+
+# creating and training the model
+max_len = 50
+
+
+def build_model():
+    bert_encoder = TFBertModel.from_pretrained(model_name)
+    input_word_ids = tf.keras.Input(shape=(max_len,), dtype=tf.int32, name="input_word_ids")
+    input_mask = tf.keras.Input(shape=(max_len,), dtype=tf.int32, name="input_mask")
+    input_type_ids = tf.keras.Input(shape=(max_len,), dtype=tf.int32, name="input_type_ids")
+
+    embedding = bert_encoder([input_word_ids, input_mask, input_type_ids])[0]
+    output = tf.keras.layers.Dense(3, activation='softmax')(embedding[:, 0, :])
+
+    model = tf.keras.Model(inputs=[input_word_ids, input_mask, input_type_ids], outputs=output)
+    model.compile(tf.keras.optimizers.Adam(lr=1e-5), loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+
+    return model
+
+
+with strategy.scope():
+    model = build_model()
+    model.summary()
+
+model.fit(train_input, train.label.values, epochs=2, verbose=1, batch_size=64, validation_split=0.2)
+
+test = pd.read_csv("/kaggle/input/contradictory-my-dear-watson/test.csv")
+test_input = bert_encode(test.premise.values, test.hypothesis.values, tokenizer)
+
+# make and submit the predictions
+predictions = [np.argmax(i) for i in model.predict(test_input)]
+submission = test.id.copy().to_frame()
+submission['prediction'] = predictions
+submission.to_csv("submissionBERT.csv", index=False)
